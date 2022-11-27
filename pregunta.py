d35e68fe54e90cc2b3b088989413bab87ec33bb4
Juanes8a <80339256+Juanes8a@users.noreply.github.com>
2022-11-26 19:14:01 -0500

    Comentarios

--- pregunta.py
+++ pregunta.py
@@ -28,16 +28,26 @@ def clean_data():
     #
     # Inserte su código aquí
     #
+    #Eliminar primera columna
     del df["Unnamed: 0"]
+
+    #Descartar nulos
     df = df.dropna()
-    df.sexo= df.sexo.str.lower()
+
+    #Estandarizar valores de las columnas a minúsculas y quitar guiones
+    df["sexo"]= df["sexo"].str.lower()
     df["tipo_de_emprendimiento"]= df["tipo_de_emprendimiento"].str.lower()
     df["línea_credito"]= df["línea_credito"].str.lower().str.replace("-"," ").str.replace("_"," ")
-    df["comuna_ciudadano"]= df["comuna_ciudadano"].astype(int)
-    df.barrio = df["barrio"].str.lower().str.replace("-"," ").str.replace("_"," ")
-    df.idea_negocio= df["idea_negocio"].str.lower().str.replace("-"," ").str.replace("_"," ")
-    df.monto_del_credito= df['monto_del_credito'].str.replace('\$[\s*]', '').str.replace(',', '').str.replace('\.00', '').astype(int)
-    df['fecha_de_beneficio'] = df.fecha_de_beneficio.apply(cambio)
-    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio,format="%d/%m/%Y")
+    df["barrio"] = df["barrio"].str.lower().str.replace("-"," ").str.replace("_"," ")
+    df["idea_negocio"]= df["idea_negocio"].str.lower().str.replace("-"," ").str.replace("_"," ")
+
+    #Se elimina el signo pesos con espacio, las comas de como divisores y el .00
+    df["monto_del_credito"]= df['monto_del_credito'].str.replace('\$[\s*]', '').str.replace(',', '').str.replace('\.00', '').astype(int)
+    
+    #Se estandariza la fecha 
+    df['fecha_de_beneficio'] = df["fecha_de_beneficio"].apply(cambio)
+    df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"],format="%d/%m/%Y")
+
+    #Descartar duplicados
     df.drop_duplicates(inplace=True)
     return df
